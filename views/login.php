803e0ff01fa4b1196492d0fc159d24af384b60ac
komiifo <nathan440@live.fr>
2023-09-27 13:30:51 +0400

    Update login.php

--- views/login.php
+++ views/login.php
@@ -15,8 +15,8 @@
 
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                     <li><a href="index.php" class="nav-link px-2 text-white">Accueil</a></li>
-                    <li><a href="#" class="nav-link px-2 text-white">Message</a></li>
-                    <li><a href="#" class="nav-link px-2 text-white">Mon profil</a></li>
+                    <li><a href="views/message.php" class="nav-link px-2 text-white">Message</a></li>
+                    <li><a href="views/profil" class="nav-link px-2 text-white">Mon profil</a></li>
                 </ul>
 
                 <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
